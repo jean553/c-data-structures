385db22428c547121411a621e251279836abfa91
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-05-03 13:13:12 +0200

    define first incomplete version of create method without index tests

--- unrolled_linked_list/unrolled_linked_list.c
+++ unrolled_linked_list/unrolled_linked_list.c
@@ -1,15 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "vector.h"
 #include "unrolled_linked_list.h"
 
 /**
  *
  */
-UnrolledLinkedList create(const int data)
+UnrolledLinkedList createULL(
+    const unsigned short size,
+    const int data
+)
 {
-    /* TODO */
+    Vector* vector = malloc(sizeof(Vector));
+    vector->size = size;
+    vector->array = (int*) malloc(sizeof(int) * size);
+
+    unsigned short i = 0;
+    for (i = 0; i < size; i++)
+    {
+        vector->array[i] = 0;
+    }
+
+    UnrolledLinkedListNode* node = malloc(sizeof(UnrolledLinkedListNode));
+    node->vector = vector;
+    node->next = NULL;
 
     UnrolledLinkedList list;
+    list.head = node;
+
     return list;
 }
