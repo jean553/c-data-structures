0830c4cc2c5cdb2dea03911c76da8220ea148832
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-05-03 12:44:43 +0200

    integrate unrolled linked list skeleton and files

---
+++ unrolled_linked_list/unrolled_linked_list.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "unrolled_linked_list.h"
+
+/**
+ *
+ */
+UnrolledLinkedList create(const int data)
+{
+    /* TODO */
+
+    UnrolledLinkedList list;
+    return list;
+}
---
+++ unrolled_linked_list/unrolled_linked_list.h
@@ -0,0 +1,27 @@
+/**
+ * @brief Unrolled link list methods
+ * @file unrolled_link_list.h
+ */
+
+typedef struct Vector Vector;
+
+typedef struct UnrolledLinkedList
+{
+    struct UnrolledLinkedListNode* head;
+} UnrolledLinkedList;
+
+typedef struct UnrolledLinkedListNode
+{
+    struct UnrolledLinkedListNode* next;
+    struct Vector* vector;
+} UnrolledLinkedListNode;
+
+/**
+ * @brief Creates a new unrolled linked list with one node
+ * and one item into the first dynamic array
+ *
+ * @param data integer to set into the new created node dynamic array
+ *
+ * @return UnrolledLinkedList
+ */
+UnrolledLinkedList create(const int data);
