75d6a6eee70e21051b936f2022c351ef7767eecd
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-05-24 21:02:40 +0200

    insert binary search tree insert method

--- binary_search_tree/binary_search_tree.c
+++ binary_search_tree/binary_search_tree.c
@@ -18,3 +18,79 @@ BinarySearchTree create(const int key)
 
     return tree;
 }
+
+/**
+ *
+ */
+void insert(
+    BinarySearchTree* tree,
+    const int key
+)
+{
+    BinarySearchTreeNode* node = tree->root;
+
+    for (;;)
+    {
+        if (node->key == key)
+        {
+            break;
+        }
+
+        if (
+            node->left == NULL &&
+            node->right == NULL
+        )
+        {
+            BinarySearchTreeNode* newNode = createNode(key);
+
+            if (node->key > key)
+            {
+                node->left = newNode;
+            } else {
+                node->right = newNode;
+            }
+
+            break;
+        }
+
+        if (node->key >= key)
+        {
+            if (node->left != NULL)
+            {
+                node = node->left;
+
+                continue;
+            }
+
+            node->left = createNode(key);
+
+            break;
+        }
+        else 
+        {
+            if (node->right != NULL)
+            {
+                node = node->right;
+
+                continue;
+            }
+
+            node->right = createNode(key);
+
+            break;
+        }
+    }
+}
+
+/**
+ *
+ */
+BinarySearchTreeNode* createNode(const int key)
+{
+    BinarySearchTreeNode* newNode = malloc(sizeof(BinarySearchTreeNode));
+    newNode->key = key;
+    newNode->left = NULL;
+    newNode->right = NULL;
+
+    return newNode;
+}
