c0bccd6c072e008883463c93f222f7b13ba586c1
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-12-01 21:15:52 +0100

    add red black tree function that checks if a node has a red parent, black uncle and if it is a right child

--- red_black_tree/red_black_tree.h
+++ red_black_tree/red_black_tree.h
@@ -94,6 +94,23 @@ const unsigned short hasRedParentAndBlackUncleAndIsLeftChild(
 );
 
 /**
+ * @brief Checks if the node has a red parent and a black uncle
+ * and if the current node is a right child node;
+ * returns 1 if yes, returns 0 if no
+ *
+ * @param grandParent the grand parent of the node to check
+ * @paran parent the parent of the node to check
+ * @param node the concerned node to check
+ *
+ * @return const unsigned short
+ */
+const unsigned short hasRedParentAndBlackUncleAndIsRightChild(
+    RedBlackTreeNode* grandParent,
+    RedBlackTreeNode* parent,
+    RedBlackTreeNode* node
+);
+
+/**
  * @brief Changes the parent and uncle color of a node to black
  *
  * @param root the root node of the whole tree
