8f1a87b5690cbec0098abc68615c46a5ea9716d8
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2018-05-05 11:22:52 +0200

    initialize merkle tree root node hash with zeros when create a new tree

--- merkle_tree/merkle_tree.c
+++ merkle_tree/merkle_tree.c
@@ -10,7 +10,15 @@ MerkleTree createMerkleTree() {
     MerkleTreeNode root;
     root.left = NULL;
     root.right = NULL;
-    root.hash = NULL;
+    root.data = 0;
+
+    for (
+        size_t index = 0;
+        index < HASH_BYTES_LENGTH;
+        index += 1
+    ) {
+        root.hash[index] = 0;
+    }
 
     MerkleTree tree;
     tree.leavesAmount = 0;
