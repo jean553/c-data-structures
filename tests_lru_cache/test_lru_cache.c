a67092172f98fd78c0e2e6ff60b49a386fe14b58
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2018-08-11 11:40:17 +0200

    integrate a first version of the lru cache tests

---
+++ tests_lru_cache/test_lru_cache.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <check.h>
+
+#include "lru_cache.h"
+#include "self_organizing_list.h"
+
+/**
+ *
+ */
+START_TEST(test_lru_cache)
+{
+   LRUCache cache = createLRU(5); 
+}
+END_TEST
+
+/**
+ *
+ */
+Suite* lru_cache_suite()
+{
+    Suite *suite = suite_create("lru_cache");
+    TCase *tcase = tcase_create("case");
+
+    tcase_add_test(tcase, test_lru_cache);
+
+    suite_add_tcase(suite, tcase);
+
+    return suite;
+}
+
+/**
+ *
+ */
+int main(int argc, char *argv[])
+{
+    Suite *suite = lru_cache_suite();
+    SRunner *runner = srunner_create(suite);
+
+    srunner_run_all(runner, CK_NORMAL);
+    int number_failed = srunner_ntests_failed(runner);
+    srunner_free(runner);
+
+    return number_failed;
+}
